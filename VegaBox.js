029edf806732b51298c5e83862c24765021edd96
Profesor Aleksej <aleksej.petricig@potep.org>
2023-03-02 15:55:49 +0100

    Priprava potrebnih knjižnic in strežnika.

---
+++ VegaBox.js
@@ -0,0 +1,13 @@
+
+if (!process.env.PORT) {
+    process.env.PORT = 8080;
+}
+
+var mime = require('mime');
+var formidable = require('formidable');
+var http = require('http');
+var fs = require('fs-extra');
+var util = require('util');
+var path = require('path');
+
+var dataDir = "./data/";
