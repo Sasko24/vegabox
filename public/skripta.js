fc68f7c792b8f219a5f3c4f0396ac58c6122b962
Sasko24 <126756431+Sasko24@users.noreply.github.com>
2023-05-30 19:12:55 +0000

    koncana skripta

--- public/skripta.js
+++ public/skripta.js
@@ -1,39 +1,32 @@
-// TODO 1.0 Dodaj EventListener za nalozitev spletne strani tipa 'load'
-window.addEventListener('/* tip dogodka */', function() {
+window.addEventListener('load', function() {
     var prizgiCakanje = function() {
-        document.querySelector('.loading').style.display = /* TODO 1.1 Dodajte vrednost za PRIKAZ elementa */;
+        document.querySelector('.loading').style.display = 'block';
     }
     var ugasniCakanje = function() {
-        document.querySelector('.loading').style.display = /* TODO 1.2 Dodajte vrednost za SKRITJE elementa */;
+        document.querySelector('.loading').style.display = 'none';
     }
-
-    // TODO 2.O Dodaj 'click' EventListener elementu z id="nalozi"
-
-    // Pridobi seznam datotek
+    if(id="nalozi"){
+        this.addEventListener(click);
+    }
+    
     var pridobiSeznamDatotek = function(event) {
-        // TODO 3.0 Kliči funkcijo prizgiCakanje
-        
+        prizgiCakanje();
         var xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function() {
             if(xhttp.readyState == 4 && xhttp.status == 200) {
                 var datoteke = JSON.parse(xhttp.responseText);
-                var datotekeHTML = document.querySelector('/* TODO 3.1 Dodaj element z ID datoteke */');
+                var datotekeHTML = document.querySelector('.datoteka');
 
                 /* TODO 3.2 Z zanko ZA pojdi skozi seznam datotek (datoteke.length) in izvedi naslednje korake:
                  *  1. določi spremenljivko, ki predstavlja datoteko
                  *  2. definiraj spremenljivko za velikost datoteke
                  *  3. definiraj spremenljivko za enoto z vrednostjo "B" (kot Byte)
                  */
-                for (/* FIXME Dopolni zanko */) {
-                    // TODO 3.2.1 Določitev spremenljivke dototeka
-                    var dodatoteka = /* Koda gre sem... */;
-
-                    // TODO 3.2.2 Določitev velikosti datoteke
-                    var velikost = /* Koda gre sem... */;
-
-                    // TODO 3.2.3 Določitev vrednosti osnovne merske enote velikosti datotek
-                    var enota = /* Koda gre sem... */
+                for (let i = 0; i < datoteke.length ; i++) {
+                    var dodatoteka = datoteka[i];
+                    var velikost = datoteka.velikost;
+                    var enota = "B";
 
                     datoteke.innerHTML += " \
                         <div class='datoteka'> \
@@ -45,37 +38,30 @@ window.addEventListener('/* tip dogodka */', function() {
                 }
 
                 if (datoteke.length > 0) {
-                    // TODO 3.3 Dodaj EventListener za dogodek click in klic funkcije brisi
-                    document.querySelector("span[akcija=brisi]").addEventListener(/* Koda gre sem... */);
+                    document.querySelector("span[akcija=brisi]").addEventListener("click", brisi());
                 }
-                // TODO 3.4 Klici funkcijo ugasniCakanje()
+                ugasniCakanje();
             }
         };
     }
 
     var brisi = function(event) {
-        // TODO 4.0 Klici funkcijo prizgiCakanje() 
+        prizgiCakanje(); 
 
         var xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function() {
 			if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-                /** 2. Preveri da lastnost responseText objekta xhttp odgovarja "Datoteka izbrisana",
-                 *  v pozitivnem primeru preusmeri uporabnika na glavno stran (pomoč: window.location)
-                 *  sicer sproži alert s sporočilom, ki sporoči uporabniku, da ni moč zbrisati datoteko
-                 */
 				if (xhttp.responseText == "Datoteka izbrisana") {
-					window.location = /* FIXME Dopolni... */;
+					window.location = "/";
 				} else {
-					// TODO 4.1 Podaj sporočilo napake
+					window.alert("Datoteke ni moč zbrisati");
 				}
-                // TODO4.2 Klici funkcijo ugasniCakanje()
+                ugasniCakanje();
 			}
 			
 		};
-        // TODO 5.0 Poglej sintakso funkcije .open() ter podaj zahtevo strežniku tipa GET
-		xhttp.open("/* FIXME Dopolni... */", "/brisi/"+this.getAttribute("datoteka"), true);
-		// TODO 5.1 Pošlji zahtevo strežniku s funkcijo send() na objektu xhttp
+		xhttp.open("GET", "/brisi/"+this.getAttribute("datoteka"), true);
+        xhttp.send();
     }
 });
